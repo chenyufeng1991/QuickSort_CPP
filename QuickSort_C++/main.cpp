c208245b37ed1a920548c5fb8f3848f6d965a38d
陈宇峰 <yufengcode@gmail.com>
2016-09-24 15:04:11 +0800

    init

--- QuickSort_C++/main.cpp
+++ QuickSort_C++/main.cpp
@@ -7,9 +7,79 @@
 //
 
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+int Partition(vector<int> &vec, int left, int right);
+void QuickSort(vector<int> &vec, int left, int right);
+
+int main(int argc, const char * argv[])
+{
+    int arr[] = {5,1,2,8,0,6};
+    vector<int> vectorFromArray(arr,arr + sizeof(arr)/sizeof(int));
+
+    QuickSort(vectorFromArray, 0, 5);
+
+    vector<int>::iterator vecIte;
+    for (vecIte = vectorFromArray.begin(); vecIte != vectorFromArray.end(); vecIte++)
+    {
+        cout << *vecIte << " ";
+    }
+
     return 0;
 }
+
+void QuickSort(vector<int> &vec, int left, int right)
+{
+    if (left == right)
+    {
+        return;
+    }
+
+    int index = Partition(vec, left, right);
+    if (index > left)
+    {
+        QuickSort(vec, left, index);
+    }
+    if (index < right)
+    {
+        QuickSort(vec, index + 1, right);
+    }
+}
+
+
+int Partition(vector<int> &vec, int left, int right)
+{
+    int i = left, j = right,x = vec[left];
+
+    while (i < j)
+    {
+        while (i < j && vec[j] >= x)
+        {
+            j--;
+        }
+        if (i < j)
+        {
+            vec[i++] = vec[j];
+        }
+
+        while (i < j && vec[i] < x)
+        {
+            i++;
+        }
+        if (i < j)
+        {
+            vec[j--] = vec[i];
+        }
+    }
+    vec[i] = x;
+
+    return i;
+}
+
+
+
+
+
